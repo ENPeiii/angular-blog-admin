8be411ab2ec89ae820304fc27825ba191c4ae3aa
ENPei <labibi.lg@gmail.com>
2026-06-30 17:05:35 +0800

    fix

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   apiBaseUrl: 'https://api.enpei.com.tw/api',
-  googleClientId: 'GOOGLE_CLIENT_ID_PLACEHOLDER',
+  googleClientId: '732918044129-fuhtv4bgqkc78clelc19anr787apa1u6.apps.googleusercontent.com',
   algolia: {
     appId: 'V30V8P3XQG',
     apiKey: '1c7ca3ac2555f470e33b5caa7e580bb3',
